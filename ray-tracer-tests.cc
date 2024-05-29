0e43ba3ecfbe72fa28aad535880ec75c840435e1
MM <mohammed.magdi999@gmail.com>
2024-05-29 23:35:55 +0300

    canvas

--- ray-tracer-tests.cc
+++ ray-tracer-tests.cc
@@ -28,7 +28,7 @@ tuple<int, int, int, int> addTuples(const tuple<int, int, int, int>& a1, const t
     tie(a2_1, a2_2, a2_3, a2_4) = a2;
     return make_tuple(a1_1 + a2_1, a1_2 + a2_2, a1_3 + a2_3, a1_4 + a2_4);
 }
-//function to subtract tuples 
+//function to subtract tuples
 tuple<int, int, int> subtractTuples(const tuple<int, int, int>& a1, const tuple<int, int, int>& a2) {
     int a1_1, a1_2, a1_3;
     int a2_1, a2_2, a2_3;
@@ -46,7 +46,7 @@ tuple<double, double, double, double> scalarMultiply(const tuple<int, int, int, 
     return make_tuple(a1_1 * 3.5, a1_2 * 3.5, a1_3 * 3.5, a1_4 * 3.5);
 
 }
-// computing magnitude of a vector 
+// computing magnitude of a vector
  double getMagnitude(const tuple<int, int, int>& a1) {
     int a1_1, a1_2, a1_3;
     //int a2_1, a2_2, a2_3;
@@ -86,7 +86,7 @@ tuple<int, int, int> crossproduct(const tuple<int, int, int>& a, const tuple<int
 double Color(const tuple<double, double ,double>& a1) {
     double a1_1, a1_2, a1_3;
     tie(a1_1, a1_2, a1_3) = a1;
-    
+
     return get<0>(a1);
 }
 //modify it later
@@ -126,7 +126,9 @@ tuple <double, double, double> hadamard_product(const tuple<double, double, doub
 
 double createCanvas(const tuple<double, double>& canvas /*const double pixel*/) {
     double width, height;
+
     tie(width, height) = canvas;
+    // include black pixels rgb(0,0,0)
 
     return get<0>(canvas);
 }
@@ -161,19 +163,19 @@ TEST(TuplesFeatures, SubtractTwoTuples) {
      tuple<int, int, int> p1 = make_tuple(3, 2, 1);
      tuple<int, int, int> p2 = make_tuple(5, 6, 7);
      tuple<int, int, int> expected_result = make_tuple(-2, -4, -6);
-     
+
      tuple<int, int, int> actual_result = subtractTuples(p1, p2);
      ASSERT_EQ(actual_result, expected_result);
-    
+
 }
 
 TEST(TuplesFeatures, MultiplyaTuple) {
-     tuple<int, int, int, int> a = make_tuple(1, -2, 3, -4); 
+     tuple<int, int, int, int> a = make_tuple(1, -2, 3, -4);
      tuple<double, double, double, double> expected_result = make_tuple(3.5, -7, 10.5, -14);
-     
+
      tuple<double, double, double, double> actual_result = scalarMultiply(a);
      ASSERT_EQ(actual_result, expected_result);
-    
+
 }
 
 TEST(TuplesFeatures, GetMagnitude) {
@@ -188,7 +190,7 @@ TEST(TuplesFeatures, GetMagnitude) {
 TEST(TuplesFeatures, NormalizeVector) {
     tuple<int, int, int> vec = make_tuple(4, 0 ,0);
     tuple<int, int, int> expected_result = make_tuple(1, 0, 0);
-    
+
     tuple<int, int, int> actual_result = normalizeVector(vec);
 
     ASSERT_EQ(actual_result, expected_result);
@@ -200,21 +202,21 @@ TEST(TuplesFeatures, Dotproduct) {
     tuple<float, float, float> vec1 = make_tuple(1, 2, 3);
     tuple<float, float, float> vec2 = make_tuple(2, 3, 4);
     float expected_result = 20;
-    
+
     float actual_result = dotProduct(vec1, vec2);
 
     ASSERT_EQ(actual_result, expected_result);
 }
 
 TEST(TuplesFeatures, Crossproduct) {
-     tuple<int, int, int> a = make_tuple(1, 2, 3); 
-     tuple<int, int, int> b = make_tuple(2, 3, 4); 
+     tuple<int, int, int> a = make_tuple(1, 2, 3);
+     tuple<int, int, int> b = make_tuple(2, 3, 4);
      tuple<int, int, int> expected_result = make_tuple(-1, 2, -1);
-     
+
      tuple<int, int, int> actual_result = crossproduct(a,b);
-     
+
      ASSERT_EQ(actual_result, expected_result);
-    
+
 }
 TEST(TuplesFeatures, CreateColor) {
     tuple<double, double, double> a1 = make_tuple(-0.5, 0.4, 1.7);
@@ -267,15 +269,23 @@ TEST(TuplesFeatures, MultiplyColorByScalar) {
 }
 
 TEST(TuplesFeatures, Hadamardproduct) {
-     tuple<double, double, double> c1 = make_tuple(1, 0.2, 0.4); 
-     tuple<double, double, double> c2 = make_tuple(0.9, 1, 0.1); 
+     tuple<double, double, double> c1 = make_tuple(1, 0.2, 0.4);
+     tuple<double, double, double> c2 = make_tuple(0.9, 1, 0.1);
      tuple<double, double, double> expected_result = make_tuple(0.9, 0.2, 0.04);
-     
+
      tuple<double, double, double> actual_result = hadamard_product(c1,c2);
 
      const double epsilon = 1e-6;
      ASSERT_NEAR(get<0>(actual_result), get<0>(expected_result), epsilon);
      ASSERT_NEAR(get<1>(actual_result), get<1>(expected_result), epsilon);
      ASSERT_NEAR(get<2>(actual_result), get<2>(expected_result), epsilon);
+
+}
+TEST(CanvasFeatures, CreateCanvas) {
+     tuple<double, double> canvas = make_tuple(10,20);
+     double expected_result = 10;
+     double actual_result = createCanvas(canvas);
+
+     ASSERT_EQ(actual_result, expected_result);
 
 }
